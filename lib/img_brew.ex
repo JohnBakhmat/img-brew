55ab1c81f064e97207ca989a3baa23b82f44ceff
John Bakhmat <johnbakhmat@gmail.com>
2023-10-03 11:19:54 +0300

    Init

---
+++ lib/img_brew.ex
@@ -0,0 +1,5 @@
+defmodule ImgBrew do
+  def hello do
+    :world
+  end
+end
---
+++ test/img_brew_test.exs
@@ -0,0 +1,8 @@
+defmodule ImgBrewTest do
+  use ExUnit.Case
+  doctest ImgBrew
+
+  test "greets the world" do
+    assert ImgBrew.hello() == :world
+  end
+end
